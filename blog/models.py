2ceea291a487bcd66bfafde3b4222714d47d4cf7
syprox <syprox4@gmail.com>
2024-12-22 16:10:46 +0200

    Added slug to category

--- blog/models.py
+++ blog/models.py
@@ -11,6 +11,7 @@ STATUS = (
 class Category(models.Model):
     category_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=30)
+    slug = models.SlugField(max_length=30, unique=False, null=True)
 
     class Meta:
         verbose_name_plural = "Розділ"
