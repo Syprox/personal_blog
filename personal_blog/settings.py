e91fe0ebbe61b137f53961e9a1606bebb3dd4464
syprox <syprox4@gmail.com>
2025-01-30 18:05:36 +0200

    debug with default

--- personal_blog/settings.py
+++ personal_blog/settings.py
@@ -28,8 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-#DEBUG = os.environ.get('DEBUG', False)
-DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+DEBUG = os.environ.get('DEBUG', False)
 
 
 ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS').split(',')
